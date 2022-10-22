08cc67a06b3392f9fd0e5e139c7d1c8112b371c7
david <david@david-PC>
2022-10-22 17:33:05 +0200

    Realizar matricula hecho. Falta login

--- Proyecto_Kronos_Data/Krono_Data/src/persistencia/MatriculaDAO.java
+++ Proyecto_Kronos_Data/Krono_Data/src/persistencia/MatriculaDAO.java
@@ -3,11 +3,18 @@ package persistencia;
 import java.util.Vector;
 import negocio.entities.Matricula;
 
-public class MatriculaDAO{
+public class MatriculaDAO {
 
 	public Vector<Object> get(String s) throws Exception {
+
 		return null;
 	}
-	
-	
+
+	public static void insert(Matricula mat) throws ClassNotFoundException {
+		String sql = "INSERT INTO Matricula (CursoId, DniAlumno, fecha, pagado) VALUES ( '" + mat.getTitulo().getId()
+				+ "', " + mat.getEstudiante().getDNI() + ", '" + mat.getFecha() + "', " + mat.isPagado() + ")";
+		GestorBD.ExecuteUpdate(sql);
+
+	}
+
 }
