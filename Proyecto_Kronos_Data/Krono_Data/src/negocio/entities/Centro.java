43b180254cacc67eb864604e2b58087bf5985392
Óscar Gómez Toledano <oscar.gomez9@alu.uclm.es>
2022-11-17 17:13:27 +0100

    Cambiado como se muestran los cursos propuestos

--- Proyecto_Kronos_Data/Krono_Data/src/negocio/entities/Centro.java
+++ Proyecto_Kronos_Data/Krono_Data/src/negocio/entities/Centro.java
@@ -12,5 +12,30 @@ public class Centro {
 		this.nombre=nombre;
 		this.localizacion=localizacion;
 		}
+	public Collection<CursoPropio> getCursoPropios() {
+		return cursoPropios;
+	}
+	public void setCursoPropios(Collection<CursoPropio> cursoPropios) {
+		this.cursoPropios = cursoPropios;
+	}
+	public Collection<ProfesorUCLM> getPlantilla() {
+		return plantilla;
+	}
+	public void setPlantilla(Collection<ProfesorUCLM> plantilla) {
+		this.plantilla = plantilla;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getLocalizacion() {
+		return localizacion;
+	}
+	public void setLocalizacion(String localizacion) {
+		this.localizacion = localizacion;
+	}
+	
 }
 
--- Proyecto_Kronos_Data/Krono_Data/src/presentacion/PantallaVerPropuestas.java
+++ Proyecto_Kronos_Data/Krono_Data/src/presentacion/PantallaVerPropuestas.java
@@ -1,93 +1,94 @@
 package presentacion;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
-import javax.swing.JFrame;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 import negocio.entities.CursoPropio;
 import negocio.entities.EstadoCurso;
-import persistencia.*;
-import presentacion.PantallaVerInfor;
+import persistencia.CursoPropioDAO;
+import negocio.controllers.GestorPropuestasCursos;
+import negocio.entities.*;
 
 public class PantallaVerPropuestas extends javax.swing.JFrame {
 
+	private static TableModel Tabla = null;
 	private JPanel contentPane;
+	private JScrollPane rollo;
+	private JTable tablaCursos;
 	private JTextPane textPane;
+	private JLabel label_2;
+	private JTextArea textFieldMotivo;
+	List<CursoPropio> cursos = CursoPropioDAO.obtenerCursosPorTipo(CursoPropioDAO.obtenerCursos(), EstadoCurso.PROPUESTO);
 	
 	public PantallaVerPropuestas() throws Exception{
 		
-		CursoPropioDAO c = new CursoPropioDAO();
-		EstadoCurso EstadoCurso = c.ComparacionEstadoCurso("PROPUESTO");
-		List<CursoPropio> cursos = CursoPropioDAO.obtenerCursos();
-		List<CursoPropio> CursosPropuestos = CursoPropioDAO.obtenerCursosPorTipo(cursos, EstadoCurso);
-
-		String[] a = new String[CursosPropuestos.size()];
-		int i=0;
-		for (CursoPropio cp : CursosPropuestos) {
-			a[i]=CursosPropuestos.get(i++).getNombre();
-		}
-		Pantalla(a);
-	}
-	private void Pantalla(String[] a) {
-		setTitle("Ver Propuestas");
+		setTitle("Cursos propuestos");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 485, 285);
+		setBounds(500, 100, 1300, 700);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		
-		JLabel label_1 = new JLabel("Estado");
-		label_1.setForeground(Color.RED);
-		label_1.setBounds(6, 140, 61, 16);
-		contentPane.add(label_1);
-		JLabel label_2 = new JLabel("Listado de cursos propuestos:");
-		label_2.setBounds(10,20,190,30);
-		contentPane.add(label_2);
-		
-		
-		textPane = new JTextPane();
-		textPane.setToolTipText("Panel para mostrar el restultado de la comprobaci\u00F3n de login o las excepciones lanzadas");
-		textPane.setEditable(false);
-		textPane.setBounds(6, 168, 400, 75);
-		contentPane.add(textPane);
-		
-		int e=40;
-		
-		
-		for(int i = 0; i < a.length; i++) {
-			JButton Bpropuesta = new JButton(a[i]);
-			Bpropuesta.setBounds(190, e, 115, 30);
-			contentPane.add(Bpropuesta);
-			e+=35;
-			Bpropuesta.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					PantallaVerInfor frame;
-					try {
-						frame = new PantallaVerInfor();
-						frame.setVisible(true);
-					} catch (Exception e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-						
-				}
-			});
-			
-			
-		}
-		
-	}
 	
+		String cabecera[] = { "Id", "Nombre", "ETCS", "Fecha Inicio", "Fecha Fin", "Tasa Matricula", "Edicion",
+				"Estado", "Tipo", "Centro", "Director", "Secretario" };
+		Tabla = new DefaultTableModel(null, cabecera);
+		tablaCursos = new JTable(Tabla);
+		rollo = new JScrollPane(tablaCursos);
+		add(rollo);
+		rollo.setBounds(6, 200, 1270, 400);
+		DefaultTableModel tabla = (DefaultTableModel) tablaCursos.getModel();
+		
+		String[] c = new String[cabecera.length];
+		SimpleDateFormat fecha = new SimpleDateFormat("yyyy-MM-dd");
+		
+		for (int i = 0; i < cursos.size(); i++) {
+			CursoPropio curso = cursos.get(i);
+			c[0] = curso.getId();
+			c[1] = curso.getNombre();
+			c[2] = String.valueOf(curso.getECTS());
+			c[3] = fecha.format(curso.getFechaInicio());
+			c[4] = fecha.format(curso.getFechaFin());
+			c[5] = String.valueOf(curso.getTasaMatricula());
+			c[6] = String.valueOf(curso.getEdicion());
+			c[7] = curso.getTipo().toString();
+			c[8] = curso.getEstado().toString();
+			c[9] = curso.getCentro().getNombre();
+			c[10] = curso.getDirector().getDNI();
+			c[11] = curso.getSecretario().getDNI();
+
+			tabla.addRow(c);
+
+		}	
+		
+		
+		
+		
+	
+			
+			
+			
+	}
+		
 }
+	
+
 
