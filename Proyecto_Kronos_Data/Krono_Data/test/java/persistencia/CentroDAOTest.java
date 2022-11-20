3a9e0e056a91274196d52e502b61af2a60c1380b
victo <victo@192.168.1.164>
2022-11-20 19:30:24 +0100

    Actualizacion JUnit en Pre-release

---
+++ Proyecto_Kronos_Data/Krono_Data/test/java/persistencia/CentroDAOTest.java
@@ -0,0 +1,17 @@
+package persistencia;
+
+import org.junit.Test;
+
+public class CentroDAOTest {
+
+	@Test
+	public void testGetOne() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testSeleccionarCentro() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
