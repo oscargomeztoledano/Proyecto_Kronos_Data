1d7840b4f9418f76bbe9196b10bf1efc5072360d
Óscar Gómez Toledano <oscar.gome9@alu.uclme.es>
2022-11-21 10:20:06 +0100

    creacion de tests vacios

---
+++ Proyecto_Kronos_Data/Krono_Data/test/java/negocio/controllers/GestorMatriculacionTest.java
@@ -0,0 +1,17 @@
+package negocio.controllers;
+
+import org.junit.Test;
+
+public class GestorMatriculacionTest {
+
+	@Test
+	public void testRealizarMatriculacion() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testRealizarPagoMatricula() {
+		throw new RuntimeException("not yet implemented");
+	}
+
+}
