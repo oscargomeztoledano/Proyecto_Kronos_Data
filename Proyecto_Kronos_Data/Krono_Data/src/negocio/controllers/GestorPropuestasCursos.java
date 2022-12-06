d583cec9080995e5dd26ceb75030118122364ccb
Daniel <danii001mg@gmail.com>
2022-12-06 11:51:55 +0100

    Control de errores de las fechas

--- Proyecto_Kronos_Data/Krono_Data/src/negocio/controllers/GestorPropuestasCursos.java
+++ Proyecto_Kronos_Data/Krono_Data/src/negocio/controllers/GestorPropuestasCursos.java
@@ -2,10 +2,12 @@ package negocio.controllers;
 
 import java.security.SecureRandom;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 
 import negocio.entities.*;
+import net.ucanaccess.util.Logger;
 import persistencia.CentroDAO;
 import persistencia.ProfesorDAO;
 import persistencia.UsuarioDAO;
@@ -18,8 +20,13 @@ public class GestorPropuestasCursos {
             int edicion, String tipocurso, String centronombre, ProfesorUCLM director, String secretarioDni,String fechaMatriculacion, Collection<Materia> materias) throws Exception {
 		
 		SimpleDateFormat fecha = new SimpleDateFormat("yyyy-MM-dd");
+		
 		Date fechaInicio = fecha.parse(fechaI);
+		Calendar calInicio = Calendar.getInstance();
+		
 		Date fechaFinal = fecha.parse(fechaFin);
+		Calendar calFin = Calendar.getInstance();
+		
 		Date fechaMatricula=fecha.parse(fechaMatriculacion);
 		Centro centro = CentroDAO.seleccionarCentro(centronombre);
 		
@@ -27,34 +34,49 @@ public class GestorPropuestasCursos {
 		
 		TipoCurso tipo = CursoPropioDAO.comparaciontipocurso(tipocurso);
 		
-		/*switch(tipoCurso) {
+		calInicio.setTime(fechaInicio);
+		long lFechaInicio = fechaInicio.getTime() + calInicio.get(Calendar.ZONE_OFFSET) + calInicio.get(Calendar.DST_OFFSET);
+		int hrInicio = (int)(lFechaInicio/3600000); //60*60*1000
+		int diasInicio = (int)hrInicio/24;
+		
+		calFin.setTime(fechaFinal);
+		long lFechaFinal = fechaFinal.getTime() + calFin.get(Calendar.ZONE_OFFSET) + calFin.get(Calendar.DST_OFFSET);
+		int hrFinal = (int)(lFechaFinal/3600000);
+		int diasFinal = (int)hrFinal/24;
+		
+		double diferenciaAnios = calFin.get(Calendar.YEAR) - calInicio.get(Calendar.YEAR);
+		double diferenciaMeses = diferenciaAnios*12 + calFin.get(Calendar.MONTH) - calInicio.get(Calendar.MONTH);
+		double diferenciaDias = diasFinal - diasInicio;
+		
+		
+		switch(tipo) {
 			case EXPERTO:
 				if (eCTS<15 && eCTS>29) {
-					System.err.println("Error. Los créditos para los cursos de tipo experto tienen que estar entre 15 y 29.");
+					Logger.log("Error. Los créditos para los cursos de tipo experto tienen que estar entre 15 y 29.");
 				}
 				break;
 				
 			case MASTER:
 				if (eCTS!=60 && eCTS!=90 && eCTS!=120) {
-					System.err.println("Error. Los créditos introducidos para másteres deben de ser 60, 90 o 120.");
+					Logger.log("Error. Los créditos introducidos para másteres deben de ser 60, 90 o 120.");
 				}
-				else if (periodo.getYears()<2 && eCTS==120) {
-					System.err.println("Error. El tiempo para los másteres de 120 ects debe de ser de al menos dos años.");
+				else if (diferenciaAnios<2 && eCTS==120) {
+					Logger.log("Error. El tiempo para los másteres de 120 ects debe de ser de al menos dos años.");
 				}
-				else if (periodo.getYears()<1 && eCTS==60) {
-					System.err.println("Error. El tiempo para los másteres de 60 ects debe de ser de al menos un año.");
+				else if (diferenciaAnios<1 && eCTS==60) {
+					Logger.log("Error. El tiempo para los másteres de 60 ects debe de ser de al menos un año.");
 				}
 				break;
 			
 			case ESPECIALISTA:
 				if (eCTS!=30 && eCTS!=59) {
-					System.err.println("Error. Los créditos introducidos para cursos de tipo especialista deben de ser 30 o 59");
+					Logger.log("Error. Los créditos introducidos para cursos de tipo especialista deben de ser 30 o 59");
 				}
-				else if (periodo.getMonths()<6 && eCTS==30) {
-					System.err.println("Error. El tiempo para los cursos de tipo especialista de 30 ects debe de ser de al menos 6 meses.");
+				else if (diferenciaMeses<6 && eCTS==30) {
+					Logger.log("Error. El tiempo para los cursos de tipo especialista de 30 ects debe de ser de al menos 6 meses.");
 				}
-				else if (periodo.getMonths()<12 && eCTS==59) {
-					System.err.println("Error. El tiempo para los cursos de tipo especialista de 59 ects debe de ser de al menos 12 meses.");
+				else if (diferenciaMeses<12 && eCTS==59) {
+					Logger.log("Error. El tiempo para los cursos de tipo especialista de 59 ects debe de ser de al menos 12 meses.");
 				}
 				
 				break;
@@ -62,7 +84,7 @@ public class GestorPropuestasCursos {
 			case FORMACION_AVANZADA:
 				
 				if (eCTS<15 && eCTS>30) {
-					System.err.println("Error. Los créditos para los cursos de tipo formacion avanzada tienen que estar entre 15 y 30.");
+					Logger.log("Error. Los créditos para los cursos de tipo formacion avanzada tienen que estar entre 15 y 30.");
 				}
 				
 				break;
@@ -70,7 +92,7 @@ public class GestorPropuestasCursos {
 			case FORMACION_CONTINUA:
 				
 				if (eCTS<3 && eCTS>14) {
-					System.err.println("Error. Los créditos para los cursos de tipo formacion continua tienen que estar entre 3 y 14.");
+					Logger.log("Error. Los créditos para los cursos de tipo formacion continua tienen que estar entre 3 y 14.");
 				}
 				
 				break;
@@ -78,7 +100,7 @@ public class GestorPropuestasCursos {
 			case MICROCREDENCIALES:
 				
 				if (eCTS<2 && eCTS>14) {
-					System.err.println("Error. Los créditos para los cursos de tipo microcredenciales tienen que estar entre 2 y 14.");
+					Logger.log("Error. Los créditos para los cursos de tipo microcredenciales tienen que estar entre 2 y 14.");
 				}
 				
 				break;
@@ -86,25 +108,28 @@ public class GestorPropuestasCursos {
 			case CORTA_DURACION:
 				
 				if (eCTS>=2) {
-					System.err.println("Error. Los créditos para los cursos de tipo corta duracion tienen que ser inferiores a 2.");
+					Logger.log("Error. Los créditos para los cursos de tipo corta duracion tienen que ser inferiores a 2.");
 				}
-				else if (periodo.getDays()>1) {
-					System.err.println("Error. El tiempo para los cursos de corta duracion debe de ser como máximo de 1 día.");
+				else if (diferenciaDias>1) {
+					Logger.log("Error. El tiempo para los cursos de corta duracion debe de ser como máximo de 1 día.");
 				}
 				
 				break;
-		}*/
+		}
 		
-		
-//		Random r = new Random();   // he cambiado a secure random por un hotspot que me ha salido en una analisis de sonar
 		SecureRandom r = new SecureRandom();
 		CursoPropio curso = new CursoPropio(String.valueOf(r.nextInt(1000)), nombre, eCTS, fechaInicio, fechaFinal, tasaMatricula, edicion,
 				tipo, EstadoCurso.PROPUESTO, centro, director, secretario,new Date(),fechaMatricula," ");
 		
 		int resultado=0;
 		for (Materia m:materias) {
-			m.setResponsable(ProfesorDAO.seleccionarProfesor(UsuarioDAO.seleccionarUsuario(m.getDniProfesor())));
-			resultado= MateriaDAO.insertarMateriaCurso(m, curso);
+			
+			if (m.getFechaInicio().after(fechaInicio) && m.getFechaFin().before(fechaFinal)) {
+				m.setResponsable(ProfesorDAO.seleccionarProfesor(UsuarioDAO.seleccionarUsuario(m.getDniProfesor())));
+				resultado= MateriaDAO.insertarMateriaCurso(m, curso);
+			}
+			else
+				Logger.log("Error. La fecha de la materia debe de estar comprendida entre la fecha incio y fin del curso");
 		}
 		
 		resultado= CursoPropioDAO.crearNuevoCurso(curso);
